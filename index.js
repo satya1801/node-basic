d2c04ac4bd438df8bdc4be8dcdf7479d57ebdf56
Satya1801 <gowtham.iss.008@gmail.com>
2021-07-17 00:21:54 +0530

    first basic node version

---
+++ index.js
@@ -0,0 +1,24 @@
+// const express = require("express");
+const mysql = require('mysql');
+const express=require('express');
+const app = express();
+
+const con = mysql.createConnection({
+  host: "localhost",
+  port:"3306",
+  user: "root",
+  password: "Satya@1801"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use("/test", (req, res) => {
+    console.log("Recieved request");
+    res.status(200).send("success");
+});
+
+app.use('/static', express.static('public'))
+app.listen(3000, () => { console.log("I am listening") });
